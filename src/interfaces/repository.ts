592a845a994851767f8df4a85c38c900fdf1f12e
Sajan Gurung <sjnkon@gmail.com>
2020-07-19 17:15:12 +1000

    update

--- src/interfaces/repository.ts
+++ src/interfaces/repository.ts
@@ -5,65 +5,65 @@ const result = {
   uuid: "{258393d8-113a-4d6e-b753-10ce24dbabe2}",
   links: {
     watchers: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/watchers",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/watchers",
     },
     branches: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/refs/branches",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/refs/branches",
     },
     tags: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/refs/tags",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/refs/tags",
     },
     commits: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/commits",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/commits",
     },
     clone: [
       {
-        href: "https://fdx-sajangurung@bitbucket.org/findex/nemo.git",
+        href: "https://fdx-sajangurung@bitbucket.org/workspace/project.git",
         name: "https",
       },
       {
-        href: "git@bitbucket.org:findex/nemo.git",
+        href: "git@bitbucket.org:workspace/project.git",
         name: "ssh",
       },
     ],
     self: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project",
     },
     source: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/src",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/src",
     },
     html: {
-      href: "https://bitbucket.org/findex/nemo",
+      href: "https://bitbucket.org/workspace/project",
     },
     avatar: {
       href: "https://bytebucket.org/ravatar/%7B258393d8-113a-4d6e-b753-10ce24dbabe2%7D?ts=default",
     },
     hooks: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/hooks",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/hooks",
     },
     forks: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/forks",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/forks",
     },
     downloads: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/downloads",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/downloads",
     },
     pullrequests: {
-      href: "https://api.bitbucket.org/2.0/repositories/findex/nemo/pullrequests",
+      href: "https://api.bitbucket.org/2.0/repositories/workspace/project/pullrequests",
     },
   },
   fork_policy: "no_forks",
-  full_name: "findex/nemo",
+  full_name: "workspace/project",
   name: "NEMO",
   project: {
     links: {
       self: {
-        href: "https://api.bitbucket.org/2.0/workspaces/findex/projects/DEF",
+        href: "https://api.bitbucket.org/2.0/workspaces/workspace/projects/DEF",
       },
       html: {
-        href: "https://bitbucket.org/findex/workspace/projects/DEF",
+        href: "https://bitbucket.org/workspace/workspace/projects/DEF",
       },
       avatar: {
-        href: "https://bitbucket.org/account/user/findex/projects/DEF/avatar/32?ts=1568247976",
+        href: "https://bitbucket.org/account/user/workspace/projects/DEF/avatar/32?ts=1568247976",
       },
     },
     type: "project",
@@ -78,26 +78,26 @@ const result = {
     name: "master",
   },
   workspace: {
-    slug: "findex",
+    slug: "workspace",
     type: "workspace",
-    name: "Findex",
+    name: "WORKSPACE",
     links: {
       self: {
-        href: "https://api.bitbucket.org/2.0/workspaces/findex",
+        href: "https://api.bitbucket.org/2.0/workspaces/workspace",
       },
       html: {
-        href: "https://bitbucket.org/findex/",
+        href: "https://bitbucket.org/workspace/",
       },
       avatar: {
-        href: "https://bitbucket.org/workspaces/findex/avatar/?ts=1543622258",
+        href: "https://bitbucket.org/workspaces/workspace/avatar/?ts=1543622258",
       },
     },
     uuid: "{b973b22c-dfa5-499c-b25d-43049bb8390a}",
   },
   has_issues: false,
   owner: {
-    username: "findex",
-    display_name: "Findex",
+    username: "workspace",
+    display_name: "WORKSPACE",
     type: "team",
     uuid: "{b973b22c-dfa5-499c-b25d-43049bb8390a}",
     links: {
@@ -108,14 +108,14 @@ const result = {
         href: "https://bitbucket.org/%7Bb973b22c-dfa5-499c-b25d-43049bb8390a%7D/",
       },
       avatar: {
-        href: "https://bitbucket.org/account/findex/avatar/",
+        href: "https://bitbucket.org/account/workspace/avatar/",
       },
     },
   },
   updated_on: "2019-09-12T00:33:18.220986+00:00",
   size: 90201063,
   type: "repository",
-  slug: "nemo",
+  slug: "project",
   is_private: true,
   description: "",
 };
