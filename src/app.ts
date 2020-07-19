e03606085ad034cfc94642ea9bf1fb79dfd65de5
Sajan Gurung <sjnkon@gmail.com>
2020-07-19 17:23:51 +1000

    support copying variables from one repository to another

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
+import { writeFile as fsWriteFile, readFile as fsReadFile, readdir as fsReaddir } from "fs";
+import { promisify } from "util";
+
 import { BitbucketService, Paginated } from "./bitbucket.service";
 import { Repository } from "./interfaces/repository";
 import { Config } from "./config";
+
+import { Environment } from "./interfaces/environment";
+import { cleanRepoName, mapStage, splitFullName } from "./util";
+import * as _ from "lodash";
+
+const writeFile = promisify(fsWriteFile);
+const readFile = promisify(fsReadFile);
+const readDir = promisify(fsReaddir);
 
 export class App {
   private bitbucket: BitbucketService;
@@ -8,34 +19,32 @@ export class App {
   constructor() {
     this.bitbucket = new BitbucketService();
   }
-  async run() {
-    const mergeReleaseTitle = "merge release";
-    const mergeReleaseBranch = "release/1.1.12";
-    const mergeReleaseDestination = "master";
+
+  /**
+   * Script to run post release
+   * Creates release prs
+   * Creates new branches
+   * Updates new branch to be default development branch
+   */
+  async runPostRelease() {
+    const title = "merge release";
+    const source = "release/1.1.12";
+    const destination = "master";
     const newBranchName = "release/1.1.13";
-    const newBranchTarget = mergeReleaseBranch;
+    const target = source;
 
     // Get all repositories
-    const repositories = await this.getAllRepositories();
+    const repositories = await this.getAllRepositories(Config.PROJECTS);
     console.log("Found repositories", repositories.length);
 
     const prPromises = repositories.map((repo: Repository) =>
-      this.bitbucket.createPullRequests({
-        title: mergeReleaseTitle,
-        repoSlug: repo.slug,
-        source: mergeReleaseBranch,
-        destination: mergeReleaseDestination,
-      })
+      this.bitbucket.createPullRequests(repo.slug, source, destination, title)
     );
     await Promise.all(prPromises);
     console.log("Finished creating pull requests");
 
     const createNewBranchPromises = repositories.map((repo: Repository) =>
-      this.bitbucket.createNewBranches({
-        repoSlug: repo.slug,
-        name: newBranchName,
-        target: newBranchTarget,
-      })
+      this.bitbucket.createNewBranches(repo.slug, target, newBranchName)
     );
     await Promise.all(createNewBranchPromises);
     console.log("Finished creating new branches");
@@ -47,18 +56,176 @@ export class App {
     console.log("Finished updating branching model");
   }
 
-  async getAllRepositories() {
-    const result = await this.getRepositories();
-    const projects = Config.PROJECTS;
+  async runDeploymentMigration() {
+    // await this.copyToFileAndGetConfigs();
+    const repositoriesToMigrateTo = await this.getAllRepositories(Config.PROJECTS_TO_MIGRATE);
+    console.log("Repositories", repositoriesToMigrateTo.length);
+
+    const repoFolderKey = "./repo-dump/";
+    const files: string[] = await readDir(repoFolderKey);
+    const parsedFiles = files.map((file) => splitFullName(file));
+
+    for (const targeRepo of repositoriesToMigrateTo) {
+      // lowerCase repo name
+      const repoName = cleanRepoName(targeRepo.name);
+      for (const file of parsedFiles) {
+        const [repoId, stage, appName, fullName] = file;
+        // name doesn't match for my findex
+        const appNameInLowerCase = appName.toLowerCase();
+        const isMyFindex = repoName === "my findex" && appNameInLowerCase === "client portal";
+        const isSameday = repoName === "sameday" && appNameInLowerCase === "same day tax";
+        const isSameRepo = repoName === appNameInLowerCase;
+        if (!isMyFindex && !isSameRepo && !isSameday) {
+          continue;
+        }
+        const fullPath = `${repoFolderKey}${fullName}`;
+        // await this.migrateRepositoryVariables(repoId, targeRepo.uuid);
+        await this.migrateEnvironmentVariables(targeRepo, stage, fullPath);
+      }
+    }
+  }
+
+  async migrateRepositoryVariables(source: string, target: string) {
+    const sourceVars = await this.bitbucket.listVariablesForRepo(source);
+    const targetVars = await this.bitbucket.listVariablesForRepo(target);
+    console.log(
+      `received: total source variables ${sourceVars.values.length}, total target variables ${targetVars.values.length}`
+    );
+    const comparator = (a: any, b: any) => a.key === b.key;
+    const uniqueVarsToCreate = _.differenceWith(sourceVars.values, targetVars.values, comparator);
+    console.log(`total variables to create ${uniqueVarsToCreate.length}`);
+    const unsecuredVars = uniqueVarsToCreate.filter((value: any) => !value.secured);
+    const createVars = unsecuredVars.map((variable: any) =>
+      this.bitbucket.createPipelineVariable(target, variable.key, variable.value, variable.secured)
+    );
+
+    await Promise.all(createVars);
+  }
+
+  async migrateEnvironmentVariables(repository: any, stage: string, fullPath: string) {
+    // check given stage exists
+    const currentEnvironment = await this.firstOrCreate(repository, stage);
+    const variables = await readFile(fullPath, { encoding: "utf-8" });
+    const originalVars = JSON.parse(variables);
+
+    const currentVars = await this.bitbucket.listEnvironmentVariables(
+      repository.slug,
+      currentEnvironment.uuid
+    );
+    console.log("Total variables in the environment", currentVars.values.length);
+
+    const createComparator = (a: any, b: any) => a.key === b.key;
+    const uniqueVarsToCreate = _.differenceWith(originalVars, currentVars.values, createComparator);
+    console.log("Total variables to create", uniqueVarsToCreate.length);
+    // this overwrites secured variables and sets them as undefined.
+    // this should filter out secured variables after running for the first time
+
+    await Promise.all(
+      uniqueVarsToCreate.map((envVar: any) => {
+        const value = envVar.secured ? "dummy" : envVar.value;
+        return this.bitbucket.createDeploymentVariable(
+          repository.slug,
+          currentEnvironment.uuid,
+          envVar.key,
+          value,
+          envVar.secured
+        );
+      })
+    );
+
+    const updateComparator = (a: any, b: any) =>
+      a.key === b.key && !(a.value || "").localeCompare(b.value || "");
+    const uniqueVarsToUpdate = _.differenceWith(currentVars.values, originalVars, updateComparator);
+    console.log("Total variables to update", uniqueVarsToUpdate.length);
+
+    await Promise.all(
+      uniqueVarsToUpdate.map((envVar: any) => {
+        const value = envVar.secured ? "dummy" : envVar.value;
+        return this.bitbucket.updateDeploymentVariable(
+          repository.slug,
+          currentEnvironment.uuid,
+          envVar.uuid,
+          envVar.key,
+          value,
+          envVar.secured
+        );
+      })
+    );
+  }
+
+  async firstOrCreate(repository: any, stage: string): Promise<Environment> {
+    const environmentType = mapStage(stage);
+    const currentEnvironments = await this.bitbucket.listEnvironments(repository.slug);
+    const currentEnvironment = currentEnvironments?.values.filter((environment) =>
+      environment.name.toLowerCase().includes(stage.toLowerCase())
+    );
+    if (currentEnvironment?.length) {
+      return currentEnvironment[0];
+    }
+
+    return await this.bitbucket.createEnvironment(repository.slug, stage, environmentType);
+  }
+
+  async copyToFileAndGetConfigs() {
+    const repositories = await this.getAllRepositories(Config.PROJECTS);
+    console.log("Repositories", repositories.length);
+    // get target repo
+    // find deployment config from the file. pattern is stage appName
+    // create environment if doesn't exist
+    // read config for that environment and createOrUpdate config
+
+    // const appStages = stages.flatMap((stage) => apps.map((app) => `${stage} ${app}`));
+    // const newRepositories = await this.getAllRepositories(Config.PROJECTS_TO_MIGRATE);
+    // console.log("New Repositories", newRepositories);
+
+    // get all source repo
+    // save all deployment config to file
+    const enviroments = await Promise.all(
+      repositories.map(async (repo) => {
+        const enviroments = await this.bitbucket.listEnvironments(repo.slug);
+        return enviroments?.values.map((value) => ({ ...value, repo_uuid: repo.uuid }));
+      })
+    );
+
+    const flattenedEnvironments = enviroments.flatMap((v) => v);
+    console.log(
+      "getting variables for",
+      flattenedEnvironments.map((r) => r?.name)
+    );
+
+    const environmentConfigs: { [key: string]: any } = {};
+    for (const environment of flattenedEnvironments) {
+      if (!environment?.uuid) {
+        continue;
+      }
+
+      const result = await this.bitbucket.listEnvironmentVariables(
+        environment?.repo_uuid,
+        environment?.uuid
+      );
+      console.log(environment.name, result.values.length);
+      const key = `${environment.repo_uuid}~${environment.name}`;
+      environmentConfigs[key] = result;
+      await writeFile(`${key}.json`, JSON.stringify(result));
+    }
+
+    return environmentConfigs;
+  }
+
+  async getAllRepositories(projects: any): Promise<Repository[]> {
+    const result = await this.bitbucket.getRepositories();
     if (result?.values) {
-      return await this.filterRepositories(result, projects);
+      if (Array.isArray(projects[0])) {
+        // multidimensional
+        return Promise.all(
+          projects.map((project: string[]) => this.filterRepositories(result, project))
+        );
+      } else {
+        return await this.filterRepositories(result, projects);
+      }
     }
 
     return [];
-  }
-
-  async getRepositories(page?: string) {
-    return await this.bitbucket.getRepositories(page);
   }
 
   async filterRepositories(
@@ -72,16 +239,17 @@ export class App {
 
     for (let i = 0; i < result.values.length; i++) {
       const repo = result.values[i];
-      if (projects.includes(repo.name)) {
-        found.push(repo);
-        if (i >= 0) {
-          projects.splice(i, 1);
+      for (let j = projects.length; j >= 0; j--) {
+        if (projects[j] === repo.name) {
+          found.push(repo);
+          projects.splice(j, 1);
         }
       }
     }
 
     if (projects.length && result.next) {
-      const nextResult = await this.getRepositories(result.page + 1);
+      const nextPage = parseInt(result.page + 1);
+      const nextResult = await this.bitbucket.getRepositories(nextPage);
       if (nextResult) {
         return this.filterRepositories(nextResult, projects, found);
       }
--- src/bitbucket.service.ts
+++ src/bitbucket.service.ts
@@ -2,17 +2,20 @@ import { Bitbucket } from "bitbucket";
 import { APIClient } from "bitbucket/src/plugins/register-endpoints/types";
 import { Repository } from "./interfaces/repository";
 import { Config } from "./config";
+import { Environment } from "./interfaces/environment";
 
+export type EnvironmentType = "Test" | "Staging" | "Production";
 export interface Paginated<T> {
   pagelen: string;
   size: string;
   page: string;
   next: string;
-  values: T[];
+  values: Array<T>;
 }
 export class BitbucketService {
   private client: APIClient;
   private workspace?: string;
+  private pageLength: number;
   private readonly configOptions = {
     baseUrl: "https://api.bitbucket.org/2.0",
     auth: {
@@ -25,34 +28,28 @@ export class BitbucketService {
   constructor() {
     this.workspace = Config.WORKSPACE;
     this.client = new Bitbucket(this.configOptions);
+    this.pageLength = 100;
   }
 
-  async getRepositories(page: string = "1"): Promise<Paginated<Repository> | undefined> {
+  async getRepositories(page: number = 1): Promise<Paginated<Repository> | undefined> {
     try {
       const { data, status } = await this.client.repositories.list({
         workspace: this.workspace,
         sort: "-updated_on",
         page,
+        pagelen: this.pageLength,
       });
       if (status === 200) {
         return data;
       }
 
-      return undefined;
+      throw new Error(`something went wrong, status is ${status}`);
     } catch (err) {
       console.log(this.parseError(err));
     }
   }
 
-  async createNewBranches({
-    repoSlug,
-    name,
-    target,
-  }: {
-    repoSlug: string;
-    name: string;
-    target: string;
-  }) {
+  async createNewBranches(repo_slug: string, target: string, name: string) {
     const body = {
       name,
       target: {
@@ -62,26 +59,16 @@ export class BitbucketService {
     try {
       await this.client.refs.createBranch({
         _body: body,
-        repo_slug: repoSlug,
+        repo_slug,
         workspace: this.workspace,
       });
     } catch (err) {
       console.log(this.parseError(err));
     }
   }
-  async createPullRequests({
-    title,
-    repoSlug,
-    source,
-    destination,
-  }: {
-    title: string;
-    repoSlug: string;
-    source: string;
-    destination: string;
-  }) {
+  async createPullRequests(repo_slug: string, source: string, destination: string, title: string) {
     const body = {
-      title: title,
+      title,
       source: {
         branch: {
           name: source,
@@ -97,7 +84,7 @@ export class BitbucketService {
     try {
       await this.client.repositories.createPullRequest({
         _body: body,
-        repo_slug: repoSlug,
+        repo_slug,
         workspace: this.workspace,
       });
     } catch (err) {
@@ -105,11 +92,11 @@ export class BitbucketService {
     }
   }
 
-  async updateBranchingModel(repoSlug: string, developmentBranch: string) {
+  async updateBranchingModel(repo_slug: string, name: string) {
     const body = {
       development: {
         use_mainbranch: false,
-        name: developmentBranch,
+        name,
       },
       production: {
         enabled: true,
@@ -142,7 +129,203 @@ export class BitbucketService {
     try {
       await this.client.repositories.updateBranchingModelSettings({
         _body: body,
-        repo_slug: repoSlug,
+        repo_slug,
+        workspace: this.workspace,
+      });
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async listDeployments(repo_slug: string) {
+    return await this.client.repositories.listDeployments({
+      repo_slug,
+      workspace: this.workspace,
+      pagelen: this.pageLength,
+    });
+  }
+
+  async getDeployments(deployment_uuid: string, repo_slug: string) {
+    await this.client.repositories.getDeployment({
+      deployment_uuid,
+      repo_slug,
+      workspace: this.workspace,
+    });
+  }
+
+  async listEnvironments(repo_slug: string): Promise<Paginated<Environment> | undefined> {
+    try {
+      const { data, status } = await this.client.repositories.listEnvironments({
+        repo_slug,
+        workspace: this.workspace,
+        pagelen: this.pageLength,
+      });
+
+      if (status === 200) {
+        return data;
+      }
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async listEnvironmentVariables(repo_slug: string, environment_uuid: string, page: number = 1) {
+    try {
+      const { data, status } = await this.client.repositories.listDeploymentVariables({
+        repo_slug,
+        environment_uuid,
+        workspace: this.workspace,
+        page,
+        pagelen: this.pageLength,
+      });
+
+      return data;
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async createDeploymentVariable(
+    repo_slug: string,
+    environment_uuid: string,
+    key: string,
+    value: string,
+    secured: boolean
+  ) {
+    try {
+      const _body = { key, value, secured };
+      const { data } = await this.client.pipelines.createDeploymentVariable({
+        _body,
+        environment_uuid,
+        repo_slug,
+        variable_uuid: "dummy", // this is dumb
+        workspace: this.workspace,
+      });
+
+      return data;
+    } catch (err) {
+      console.log(err);
+      console.log(this.parseError(err));
+    }
+  }
+
+  async updateDeploymentVariable(
+    repo_slug: string,
+    environment_uuid: string,
+    variable_uuid: string,
+    key: string,
+    value: string,
+    secured: boolean
+  ) {
+    try {
+      const _body = { key, value, secured };
+      const { data } = await this.client.pipelines.updateDeploymentVariable({
+        _body,
+        environment_uuid,
+        repo_slug,
+        variable_uuid,
+        workspace: this.workspace,
+      });
+
+      return data;
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async updateEnvironmentVariables(
+    repo_slug: string,
+    environment_uuid: string,
+    variable_uuid: string
+  ) {
+    try {
+      const _body = {};
+      const { data } = await this.client.repositories.updateDeploymentVariable({
+        _body,
+        environment_uuid,
+        repo_slug,
+        variable_uuid,
+        workspace: this.workspace,
+      });
+
+      return data;
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async listVariablesForRepo(repo_slug: string, page: string = "1") {
+    try {
+      const { data, error } = await this.client.pipelines.listVariablesForRepo({
+        repo_slug,
+        workspace: this.workspace,
+        page,
+        pagelen: this.pageLength,
+      });
+      return data;
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async createPipelineVariable(repo_slug: string, key: string, value: string, secured: boolean) {
+    try {
+      const _body = { key, value, secured };
+      const { data, headers } = await this.client.repositories.createPipelineVariable({
+        _body,
+        repo_slug,
+        workspace: this.workspace,
+      });
+      return data;
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async update(repo_slug: string, variable_uuid: string) {
+    try {
+      const _body = {};
+      const { data, headers } = await this.client.pipelines.updateVariable({
+        _body,
+        repo_slug,
+        variable_uuid,
+        workspace: this.workspace,
+      });
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async createEnvironment(repo_slug: string, name: string, environmentType: EnvironmentType) {
+    try {
+      const _body = {
+        environment_type: {
+          type: "deployment_environment_type",
+          name: environmentType,
+        },
+        name,
+      };
+      const { data, status, error } = await this.client.deployments.createEnvironment({
+        _body,
+        repo_slug,
+        workspace: this.workspace,
+      });
+      console.log(status, error);
+      return data;
+    } catch (err) {
+      console.log(this.parseError(err));
+    }
+  }
+
+  async updateEnvironment(repo_slug: string, environment_uuid: string, name: string) {
+    try {
+      const _body = {
+        name,
+      };
+      const { data, headers } = await this.client.deployments.updateEnvironment({
+        _body,
+        environment_uuid,
+        repo_slug,
         workspace: this.workspace,
       });
     } catch (err) {
@@ -151,6 +334,7 @@ export class BitbucketService {
   }
 
   private async parseError(response: any) {
-    return response.error?.error?.message;
+    const { message, error, headers, request, status } = response;
+    return error;
   }
 }
