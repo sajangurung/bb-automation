2ebaf3c15f57f5aa2a138a07b392a9a7e77b6372
Sajan Gurung <sjnkon@gmail.com>
2020-08-01 11:58:03 +1000

    update index to run prerelease hook

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { PostReleaseTrigger, PostReleaseConfig } from "./src/post-release-hook";
+import { PreReleaseHook, PreReleaseConfig } from "./src/pre-release-hook";
 
 const postReleaseConfig: PostReleaseConfig = {
   title: "merge release",
@@ -8,5 +9,23 @@ const postReleaseConfig: PostReleaseConfig = {
   target: "release/1.1.12",
 };
 
-const trigger = new PostReleaseTrigger();
-trigger.run(postReleaseConfig);
+// const trigger = new PostReleaseTrigger();
+// trigger.run(postReleaseConfig);
+
+const preReleaseConfig: PreReleaseConfig = {
+  source: {
+    repo: "test-project",
+    name: "Test",
+    type: "Test",
+  },
+  target: {
+    repo: "test-project-2",
+    name: "Staging",
+    type: "Staging",
+  },
+  writeToFile: true,
+  deleteInTarget: true,
+  readFromFile: "./repo-dump/{679bde9a-eff3-4dab-8b11-ff9b7ec05fb5}~test-project~Test.json",
+};
+const trigger = new PreReleaseHook();
+trigger.run(preReleaseConfig);
